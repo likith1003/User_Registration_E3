0a72e01c91aab6f95f18c5186f9d9302f04b6dc1
likith1003 <123715227+likith1003@users.noreply.github.com>
2024-05-21 09:11:46 +0530

    Add files via upload

--- project13/app/views.py
+++ project13/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from app.forms import *
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -19,6 +20,16 @@ def register(request):
             MPFDO = PFDO.save(commit=False)
             MPFDO.username=MUFDO
             MPFDO.save()
+            message = f"Hello {UFDO.cleaned_data.get('first_name')} Your Registration agaainst our application is Successfull \n \n Thanks & Regards Team"
+            email = UFDO.cleaned_data.get('email')
+            send_mail(
+                'Registration Successfull',
+                message,
+                'likith.qsp@gmail.com',
+                [email],
+                fail_silently=False
+
+            )
             return HttpResponse('registration is Done')
         return HttpResponse('Invalid Data')
     return render(request, 'register.html', d)
--- project13/project13/settings.py
+++ project13/project13/settings.py
@@ -123,3 +123,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#------------------------------------------
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'likith.qsp@gmail.com'
+EMAIL_HOST_PASSWORD = 'sknq ypky btlp wdly'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
